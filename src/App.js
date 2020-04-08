6851534dd804e3984ee85d9301fdda19f990fecd
Zachary Hislop <hislopzach@gmail.com>
2020-04-07 22:22:08 -0700

    use dark theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
-import axios from "axios";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Favorites from "./Favorites";
@@ -9,30 +10,39 @@ import Favorites from "./Favorites";
 function App() {
   const clientId = "e161d1dc280f418fa572dc25328e38d3";
   const redirectUrl = "https://zachspotify.herokuapp.com/favorites";
+  const theme = React.useMemo(() =>
+    createMuiTheme({
+      palette: {
+        type: "dark",
+      },
+    })
+  );
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/favorites">
-            <Favorites />
-          </Route>
-          <Route path="/">
-            <header className="App-header">
-              <Typography variant="h2" component="h2" gutterBottom>
-                Spotify Favorites Viewer
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                href={`https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=user-top-read&response_type=token`}
-              >
-                Authenticate with Spotify
-              </Button>
-            </header>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/favorites">
+              <Favorites />
+            </Route>
+            <Route path="/">
+              <header className="App-header">
+                <Typography variant="h2" component="h2" gutterBottom>
+                  Spotify Favorites Viewer
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  href={`https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=user-top-read&response_type=token`}
+                >
+                  Authenticate with Spotify
+                </Button>
+              </header>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
