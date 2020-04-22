3aa5304f2f62c810dcf67637194be2ea58e110e0
Zachary Hislop <hislopzach@gmail.com>
2020-04-21 23:44:33 -0700

    use cards for first 5 tracks, then list items

--- src/Tracks.js
+++ src/Tracks.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import TrackCard from "./TrackCard";
+import TrackListItem from "./TrackListItem";
 
 const Tracks = ({ tracks }) => {
   return (
     <>
-      {tracks.items.map((t, index) => (
-        <Grid key={t.uri} item xs="auto">
-          <TrackCard track={t} index={index} />
-        </Grid>
-      ))}
+      {tracks.items.map((t, index) => {
+        if (index <= 4) {
+          return (
+            <Grid key={t.uri} item xs="auto">
+              <TrackCard track={t} rank={index + 1} />
+            </Grid>
+          );
+        } else {
+          return (
+            <Grid key={t.uri} item xs={11} md={10}>
+              <TrackListItem track={t} rank={index + 1} />
+            </Grid>
+          );
+        }
+      })}
+      )}
     </>
   );
 };
