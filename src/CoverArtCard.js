6ee9ddd5b30e864d5c955182f41b4656639e5bf3
Zachary Hislop <hislopzach@gmail.com>
2020-04-21 22:40:18 -0700

    create cover art card that trakc and artists will inherit from

---
+++ src/CoverArtCard.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  IconButton,
+  Collapse,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  card: {
+    width: 320,
+    margin: "auto",
+    marginBottom: "0.5em",
+    transition: "0.3s",
+    boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+    "&:hover": {
+      boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
+    },
+  },
+  cardDetails: {
+    paddingBottom: 0,
+  },
+  media: {
+    width: 320,
+    height: 320,
+  },
+}));
+
+const CoverArtCard = ({ name, description, image, rank }) => {
+  const [expanded, setExpanded] = useState(false);
+  const styles = useStyles();
+
+  return (
+    <Card className={styles.card}>
+      <CardMedia className={styles.media}>
+        <img className={styles.media} src={image} alt={name}></img>
+      </CardMedia>
+      <CardContent className={styles.cardContent}>
+        <Typography variant="h5">{`#${rank + 1} ${name}`}</Typography>
+        <IconButton
+          className={expanded ? styles.expandOpen : styles.expand}
+          onClick={() => setExpanded((prev) => !prev)}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+        <Collapse in={expanded}>
+          <Typography variant="subtitle1">{description}</Typography>
+        </Collapse>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CoverArtCard;
