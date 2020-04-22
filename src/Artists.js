dee37ed0e4cf5e97cbb982ece9f5b9cf13a86b82
Zachary Hislop <hislopzach@gmail.com>
2020-04-22 00:01:42 -0700

    display top ten artists as cards, the rest as lists

--- src/Artists.js
+++ src/Artists.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import ArtistCard from "./ArtistCard";
+import ArtistListItem from "./ArtistListItem";
 
 const Artists = ({ artists }) => {
   return (
     <>
-      {artists.items.map((a, index) => (
-        <Grid key={a.uri} item xs="auto">
-          <ArtistCard artist={a} index={index} />
-        </Grid>
-      ))}
+      {artists.items.map((a, index) => {
+        if (index < 10) {
+          return (
+            <Grid key={a.uri} item xs="auto">
+              <ArtistCard artist={a} rank={index + 1} />
+            </Grid>
+          );
+        } else {
+          return (
+            <Grid key={a.uri} item xs={11} md={10}>
+              <ArtistListItem artist={a} rank={index + 1} />
+            </Grid>
+          );
+        }
+      })}
     </>
   );
 };
