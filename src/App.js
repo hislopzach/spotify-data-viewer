dd64dd85a2ad8c0b30b971d1b8fc2053673382b1
Zachary Hislop <hislopzach@gmail.com>
2020-04-25 14:24:20 -0700

    add container around app. Set up defautls for React query caching. use welcome component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
-import { Button } from "@material-ui/core";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+import { Container } from "@material-ui/core";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+import { ReactQueryConfigProvider } from "react-query";
 import Favorites from "./Favorites";
+import Welcome from "./Welcome";
+
+import "./App.css";
+
+const reactQueryConfig = {
+  staleTime: 60 * 1000 * 10,
+  refetchOnWindowFocus: false,
+  cacheTime: 1000 * 60 * 20,
+};
+
+const useStyles = makeStyles((theme) => ({
+  app: {
+    background: "#282c34",
+  },
+}));
 
 function App() {
   const clientId = "e161d1dc280f418fa572dc25328e38d3";
   const redirectUrl = "https://spotify-favorites-viewer.web.app/favorites";
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=user-top-read&response_type=token`;
   const theme = createMuiTheme({
     palette: {
       type: "dark",
     },
   });
-
+  const styles = useStyles(theme);
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/favorites">
-              <Favorites />
-            </Route>
-            <Route path="/">
-              <header className="App-header">
-                <Typography variant="h2" component="h2" gutterBottom>
-                  Spotify Favorites Viewer
-                </Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  href={`https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=user-top-read&response_type=token`}
-                >
-                  Authenticate with Spotify
-                </Button>
-              </header>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      <ReactQueryConfigProvider config={reactQueryConfig}>
+        <div className={styles.app}>
+          <Container maxWidth="lg">
+            <Router>
+              <Switch>
+                <Route path="/favorites">
+                  <Favorites />
+                </Route>
+                <Route path="/">
+                  <Welcome url={authUrl} />
+                </Route>
+              </Switch>
+            </Router>
+          </Container>
+        </div>
+      </ReactQueryConfigProvider>
     </ThemeProvider>
   );
 }
