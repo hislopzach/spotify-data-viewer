c2da7f05373e83eaaf2e666f474c48d748be4017
Zachary Hislop <hislopzach@gmail.com>
2020-04-25 14:25:53 -0700

    remove list itme and track list item

--- src/TrackListItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ListItem from "./ListItem";
-
-const TrackListItem = ({ track, rank }) => {
-  const description = `${track.artists[0].name} - ${track.album.name}`;
-  return <ListItem name={track.name} rank={rank} description={description} />;
-};
-
-export default TrackListItem;
