abe89c0aa2765b4569d842ffab8face4bd65486e
Zachary Hislop <hislopzach@gmail.com>
2020-04-25 14:01:46 -0700

    Use cards for all artists, not just top ten

--- src/Artists.js
+++ src/Artists.js
@@ -6,21 +6,11 @@ import ArtistListItem from "./ArtistListItem";
 const Artists = ({ artists }) => {
   return (
     <>
-      {artists.items.map((a, index) => {
-        if (index < 10) {
-          return (
-            <Grid key={a.uri} item xs="auto">
-              <ArtistCard artist={a} rank={index + 1} />
-            </Grid>
-          );
-        } else {
-          return (
-            <Grid key={a.uri} item xs={11} md={10}>
-              <ArtistListItem artist={a} rank={index + 1} />
-            </Grid>
-          );
-        }
-      })}
+      {artists.items.map((a, index) => (
+        <Grid key={a.uri} item xs="auto">
+          <ArtistCard artist={a} rank={index + 1} />
+        </Grid>
+      ))}
     </>
   );
 };
