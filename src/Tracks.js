d891dec6c02f9b7f966588731f1fcd08f4c4a26e
Zachary Hislop <hislopzach@gmail.com>
2020-04-25 14:13:23 -0700

    display cards for all tracks

--- src/Tracks.js
+++ src/Tracks.js
@@ -6,21 +6,11 @@ import TrackListItem from "./TrackListItem";
 const Tracks = ({ tracks }) => {
   return (
     <>
-      {tracks.items.map((t, index) => {
-        if (index < 10) {
-          return (
-            <Grid key={t.uri} item xs="auto">
-              <TrackCard track={t} rank={index + 1} />
-            </Grid>
-          );
-        } else {
-          return (
-            <Grid key={t.uri} item xs={11} md={10}>
-              <TrackListItem track={t} rank={index + 1} />
-            </Grid>
-          );
-        }
-      })}
+      {tracks.items.map((t, index) => (
+        <Grid key={t.uri} item xs="auto">
+          <TrackCard track={t} rank={index + 1} />
+        </Grid>
+      ))}
       )}
     </>
   );
