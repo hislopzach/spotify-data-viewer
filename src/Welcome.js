bbc43add1c9dc763c82cd83539628c6570e55e00
Zachary Hislop <hislopzach@gmail.com>
2020-04-25 16:28:35 -0700

    center welcome text

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,7 @@ import { Button } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 
 const Welcome = ({ url }) => (
-  <header className="App-header">
+  <header className="App-header" style={{ textAlign: "center" }}>
     <Typography variant="h2" component="h2" gutterBottom>
       Spotify Favorites Viewer
     </Typography>
