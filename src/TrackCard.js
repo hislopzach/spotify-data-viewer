b133c9e4c2408c4aaa924f59413b855ebdba41b5
Zachary Hislop <hislopzach@gmail.com>
2021-01-21 22:57:48 -0800

    safely get artist name

--- src/TrackCard.js
+++ src/TrackCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import CoverArtCard from "./CoverArtCard";
 
 const TrackCard = ({ track, rank }) => {
-  const name = `${track.name} - ${track.artists?.length[0]?.name}`;
+  const name = `${track.name} - ${track?.artists[0]?.name}`;
   const image =
     track?.album?.images?.length > 1 ? track.album.images[1].url : null;
 
