b21f87842af374729b9b33a15a739a93e2e095a8
Zachary Hislop <hislopzach@gmail.com>
2021-01-21 22:54:55 -0800

    add react query dev tools

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import { Container } from "@material-ui/core";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ReactQueryConfigProvider } from "react-query";
 import { authUrl } from "./config";
 import Favorites from "./Favorites";
 import Welcome from "./Welcome";
 
 import "./App.css";
-
-const reactQueryConfig = {
-  staleTime: 60 * 1000 * 10,
-  refetchOnWindowFocus: false,
-  cacheTime: 1000 * 60 * 20,
-  retry: 1,
-};
 
 function App() {
   const theme = createMuiTheme({
@@ -26,20 +18,18 @@ function App() {
   });
   return (
     <ThemeProvider theme={theme}>
-      <ReactQueryConfigProvider config={reactQueryConfig}>
-        <Container maxWidth="lg">
-          <Router>
-            <Switch>
-              <Route path="/favorites">
-                <Favorites url={authUrl} />
-              </Route>
-              <Route path="/">
-                <Welcome url={authUrl} />
-              </Route>
-            </Switch>
-          </Router>
-        </Container>
-      </ReactQueryConfigProvider>
+      <Container maxWidth="lg">
+        <Router>
+          <Switch>
+            <Route path="/favorites">
+              <Favorites url={authUrl} />
+            </Route>
+            <Route path="/">
+              <Welcome url={authUrl} />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
     </ThemeProvider>
   );
 }
